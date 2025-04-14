8843306dea2cd2019f4d32fba45c4c0c58217b6a
Walter Junior <walter.jr@sempreceub.com>
2025-04-13 21:05:03 -0300

    inclusão do ".br" no email de envio da assistencia

--- src/assistencia.js
+++ src/assistencia.js
@@ -6,7 +6,7 @@ document.getElementById("assistencia-form").addEventListener("submit", function(
     const problema = document.getElementById("problema").value;
     const mensagem = document.getElementById("mensagem").value;
 
-    const mailtoLink = `mailto:walter.jr@outlook.com?subject=Ajuda com ${problema}&body=Nome: ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
+    const mailtoLink = `mailto:walter.jr@outlook.com.br?subject=Ajuda com ${problema}&body=Nome: ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
 
     window.location.href = mailtoLink;
 });
